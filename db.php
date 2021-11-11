b9cc21993b502e40ae388f0ed7317bb80b10b344
LamHoang-01 <nhlam011019@gmail.com>
2021-11-11 21:02:44 +0700

    add pw

--- db.php
+++ db.php
@@ -5,7 +5,7 @@
 
     public static function getInstance() {
       if (!isset(self::$instance)) {
-        self::$instance = mysqli_connect("localhost", "root", "password", "restaurant");
+        self::$instance = mysqli_connect("localhost", "root", "LamHoangmysql560Dtvvn", "restaurant");
       }
       return self::$instance;
     }
